4b1bfce65714a0ebb325bc051fc786b03255adf2
Thiago <thgps01@outlook.com>
2024-04-07 22:28:59 -0300

    feat: change to variables with type

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,4 @@
+import { env } from '@/@types/env'
 import { PrismaClient } from '@prisma/client'
 
 declare global {
@@ -5,7 +6,7 @@ declare global {
 }
 
 let prisma: PrismaClient
-if (process.env.NODE_ENV === 'production') {
+if (env.NODE_ENV === 'production') {
   prisma = new PrismaClient()
 } else {
   if (!global.cachedPrisma) {
