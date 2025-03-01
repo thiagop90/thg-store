0c71e11e3947c3ee567587c347d23bad4aaa82e1
Thiago <thgps01@outlook.com>
2025-03-01 13:26:57 -0300

    style: ui changes

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -13,7 +13,7 @@ export const Icons = {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="1.75"
+      stroke-width="1.5"
       stroke-linecap="round"
       stroke-linejoin="round"
     >
@@ -33,7 +33,7 @@ export const Icons = {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="1.75"
+      stroke-width="1.5"
       stroke-linecap="round"
       stroke-linejoin="round"
       {...props}
