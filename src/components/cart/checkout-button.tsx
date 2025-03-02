64d3925f841dc7c34b6ffa91ea6f1ccff5ce063b
Thiago <thgps01@outlook.com>
2025-03-02 01:00:08 -0300

    style: ui changes

--- src/components/cart/checkout-button.tsx
+++ src/components/cart/checkout-button.tsx
@@ -17,7 +17,7 @@ export function CheckoutButton() {
   const handleFinishPurchaseClick = async () => {
     setIsProcessing(true)
 
-    const order = await createOrder(cart, data?.user.id ?? '')
+    const order = await createOrder(cart, data?.user?.id ?? '')
     const checkout = await createCheckout(cart, order.id)
     const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)
 
